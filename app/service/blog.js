09735e8313ed96f3c4f6defce16a12920790259d
xingbaifang <xbf321@gmail.com>
2024-11-12 17:42:02 +0800

    Update blog.js

--- app/service/blog.js
+++ app/service/blog.js
@@ -78,7 +78,7 @@ class BlogService extends Service {
   // 实时获取 Post content blocks
   // 且如果图片是 Notion 上传，非自建图床，需要下载到本地，然后同步到图床
   async realtimeFetchNotionPageContentToDatabaseByPostId(pageId) {
-    const { POST_CONTENT } = this.DB_NAME_KEYS;
+    const { POST_CONTENT, POST_PROPERTY, PAGE_PROPERTY } = this.DB_NAME_KEYS;
     let blocks = [];
     try {
       blocks = await this.ctx.service.notion.retrieveBlockChildren(pageId);
@@ -88,6 +88,8 @@ class BlogService extends Service {
       // 为什么出现这种情况？
       // 在 Notion 中已经删除，但本地数据还有数据
       // 解决方案是，出现这种情况，删除本地数据
+      this.deleteDatabaseRowByName(`${POST_PROPERTY}/${pageId}`);
+      this.deleteDatabaseRowByName(`${PAGE_PROPERTY}/${pageId}`);
       this.deleteDatabaseRowByName(`${POST_CONTENT}/${pageId}`);
     }
     if (blocks.length === 0) {
